80218bb2b433008ad5752664decbaea83fb15218
BrettF5 <brettfort@hotmail.com>
2023-11-29 23:07:57 -0800

    Updating a few bugs

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,11 +1,11 @@
-const HornedBeast = ({ title, imageUrl, description }) => {
+function HornedBeast({ key, title, imageUrl, description }) {
     return (
-      <div>
+      <div key={key}>
         <h2>{title}</h2>
         <img src={imageUrl} alt={title} />
         <p>{description}</p>
       </div>
     );
-  };
+  }
   
   export default HornedBeast;
