3eeb0382144ca63d6537b1d8a8203ffcfcfd3227
BrettF5 <brettfort@hotmail.com>
2023-11-29 22:20:59 -0800

    Requirement for Lab 1 completed, I hope...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Gallery from './components/Gallery'
+import Footer from './components/Footer'
+
 function App() {
-
-    return <h1>I live</h1>
-
-
+    return (
+        <>
+        <Header title="Gallery of Horns" />
+        <Gallery />
+        <Footer copyright="Brett Fort 2023" />
+        </>
+    );
 }
 
 export default App;
+
