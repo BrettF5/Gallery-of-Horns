1c0c6ab3d68cfe4673c742a6abb5827c565bae17
BrettF5 <brettfort@hotmail.com>
2023-11-30 21:53:51 -0800

    Got most of Lab2 requirement done. No Hearts, row need some work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
-import Header from './components/Header'
-import Gallery from './components/Gallery'
-import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+import hornAnimals from './data.json';
+import Container from 'react-bootstrap/Container';
 
 function App() {
     return (
-        <>
+        <Container>
         <Header title="Gallery of Horns" />
-        <Gallery />
+        <Gallery message="What's your favorite horned animal" hornAnimals={hornAnimals} />
         <Footer copyright="Brett Fort 2023" />
-        </>
+        </Container>
     );
 }
 
